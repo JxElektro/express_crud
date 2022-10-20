71e8fbd729bf90e5cfb582aa31aef268c3ab58b9
Jhen Nuñez <jxelektro@gmail.com>
2022-10-20 01:47:47 -0300

    .ENV ADDED

--- config/database.js
+++ config/database.js
@@ -1,16 +1,21 @@
 import { Sequelize } from "sequelize";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const db = new Sequelize(
-    "Crud", // Nombre de Base De datos a la que apuntamos en Postgres
-    "postgres", //Nombre de Usuario Postgres
-    "alcap1990", { // Contraseña de Usuario Postgres
-    host: "localhost", // Host de la base de datos en este caso es local
-    dialect: 'postgres', // Tipo de base de datos
-    logging: false,// Para que no muestre los logs de las consultas (opcional) muy largo en bash
-    port: 5432, // Puerto de la base de datos en este caso es el 5432 por defecto de postgres
-    define: {
-        timestamps: false // Para que no se cree la columna createdAt y updatedAt creadas por defecto por sequelize
-    }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  logging: false,// Para que no muestre los logs de las consultas (opcional) muy largo en bash
+  port: process.env.DB_PORT, // Puerto de la base de datos en este caso es el 5432 por defecto de postgres
+  define: {
+      timestamps: false // Para que no se cree la columna createdAt y updatedAt creadas por defecto por sequelize
+  }
 });
+
+
 
 export default db; // Exportamos la conexion para poder usarla en otros archivos .
